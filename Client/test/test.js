b3ca940ca2061bb2d7a62cb1bebbb753c5deb7a9
Ramon Brand <ramon2009@gmail.com>
2018-01-26 14:41:25 +0200

    Added test framework and updated client readme.

---
+++ Client/test/test.js
@@ -0,0 +1,10 @@
+const assert = require("assert")
+
+describe("Utilities", function() {
+	describe("ReverseString", function() {
+		it("Should return the reverse of the given string.", function() {
+			// -- Right argument must equal left argument to pass test.
+			assert.equal("myFuncCall", "myFuncCall")
+		})
+	})
+})
