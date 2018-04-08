c67f53b1c6435b02605611d906c3b8ed7396fa5f
aelittaezugbaya <aelittae@gmail.com>
2018-04-08 18:21:06 +0300

    minor mistakes

--- MobileClient/index.js
+++ MobileClient/index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from "react-native";
+import App from "./components/App";
 
-AppRegistry.registerComponent('AwesomeProject', () => App);
+AppRegistry.registerComponent("AwesomeProject", () => App);
